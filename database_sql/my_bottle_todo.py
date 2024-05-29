1d8b2477ada4462226b127d2768d4ddea17da77b
PhMaly <philemon.maly@le-campus-nemrique.fr>
2024-05-29 13:23:01 +0200

    feat BDD connection and data insertion

--- database_sql/my_bottle_todo.py
+++ database_sql/my_bottle_todo.py
@@ -5,13 +5,13 @@ import identify as identity
 
 
 def connect_db():
-    # Connect to MariaDB Platform
     try:
         conn = mariadb.connect(
             user=identity.user(),
             password=identity.password(),
             host="localhost",
             port=3306,
+            database='todoPython'
         )
         return conn
     except mariadb.Error as e:
@@ -19,13 +19,52 @@ def connect_db():
         sys.exit(1)
 
 
-@route('/todo')
-def todo_list():
-    conn = mariadb.connect('todo.db')
-    c = conn.cursor()
-    c.execute("SELECT id, task FROM todo LIKE '1'")
-    result = c.fetchall()
-    return str(result)
+def setup_database(con):
+    try:
+        cur = con.cursor()
+        cur.execute(
+            "CREATE table if not exists todoPython.todo (id integer not null auto_increment, task varchar(100), status bool, primary key(id))engine=InnoDB")
+        cur.close()
+
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
 
 
-run(host='localhost', port=8080, debug=True)
+def insert_into_todo_table(con):
+    try:
+        print("Connecting to the database...")
+        cur = con.cursor()
+        print("Connected to the database.")
+
+        print("Inserting data...")
+        cur.execute(
+            "INSERT INTO todo (task,status) VALUES ('Read A-byte-of-python to get a good introduction into Python',0)")
+        cur.execute("INSERT INTO todo (task,status) VALUES ('Visit the Python website',1)")
+        cur.execute(
+            "INSERT INTO todo (task,status) VALUES ('Test various editors for and check the syntax highlighting',1)")
+        cur.execute("INSERT INTO todo (task,status) VALUES ('Choose your favorite WSGI-Framework',0)")
+        print("Data inserted.")
+
+        print("Committing the transaction...")
+        con.commit()
+        print("Transaction committed.")
+        cur.close()
+
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+
+
+def main():
+    con = connect_db()
+    setup_database(con)
+    insert_into_todo_table(con)
+    run(host='localhost', port=8080, debug=True)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
